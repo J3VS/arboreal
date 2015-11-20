4b293cf2b5245e97fe2c6eea42228d52eb4fa63a
Jonathan Viccary <jonathan.viccary@gmail.com>
2015-11-20 00:45:27 -0500

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to circus
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/circus/core_test.clj
@@ -0,0 +1,7 @@
+(ns circus.core-test
+  (:require [clojure.test :refer :all]
+            [circus.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
