9828ecc8de3f3f5433edce21daa786b6ac5febc6
J3VS <jonathan.viccary@gmail.com>
2017-01-09 00:08:01 -0500

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject circus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject arboreal "0.1.0-SNAPSHOT"
+  :description "Limiting side effects for the greater good"
+  :url "https://github.com/J3VS/arboreal"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
